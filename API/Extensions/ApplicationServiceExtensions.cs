695864b6127adb8bb19b976b3c57d4d3dd8dfcc7
Esct26 <sebastiancon.torres@gmail.com>
2025-06-16 08:59:49 -0600

    End of section 17

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -5,6 +5,7 @@ using API.Entities;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -37,6 +38,10 @@ public static class ApplicationServiceExtensions
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+
+        services.AddSignalR();
+
+        services.AddSingleton<PresenceTracker>();
 
         return services;
     }
